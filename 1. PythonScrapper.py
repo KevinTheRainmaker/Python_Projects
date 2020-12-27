ebeb964e896c1cce898a9e67a508b89b8d79f135
KangbeenKo <sweetrainforyou@gmail.com>
2020-12-28 01:59:27 +0900

    Job title and Company name

--- 1. PythonScrapper.py
+++ 1. PythonScrapper.py
@@ -29,8 +29,16 @@ def extract_indeed_jobs(last_page):
         results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
         for result in results:
             title = result.find("h2",{"class":"title"}).find("a")["title"]
-            print(title)
+            company = result.find("span", {"class":"company"})
+            company_anchor = company.find("a")
+            if company_anchor is not None:
+                company = company_anchor.string
+            else:
+                company = company.string
+            company = company.strip()
+            print(title , company)
     return jobs
+   
 last_page = extract_indeed_pages()
 #print(last_page)
 extract_indeed_jobs(last_page)
