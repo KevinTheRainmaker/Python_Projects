c1b93942053e94db6b7fd4547106ec5ed378c549
KangbeenKo <sweetrainforyou@gmail.com>
2020-12-28 01:42:47 +0900

    merge main and indeed

--- 1. PythonScrapper.py
+++ 1. PythonScrapper.py
@@ -1,22 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-indeed_results = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50")
+LIMIT = 50
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
 
-indeed_soup = BeautifulSoup(indeed_results.text, "html.parser")
 
-pagination = indeed_soup.find("div", {"class":"pagination"})
+def extract_indeed_pages():
+    results = requests.get(URL)
 
-links = pagination.find_all('a')
+    soup = BeautifulSoup(results.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+    max_page = pages[-1]
 
-pages = []
+    return max_page
+    # for n in range(max_page):
+    #     print(INDEED_URL +"&"+ f"start={n*50}")
 
-for link in links[:-1]:
-    pages.append(int(link.string)) 
-    #int() => to integer / TypeError: int() argument must be a string, a bytes-like object or a number, not 'NoneType' >> 'Cause of 'Next'
-    # links[]:-1]
 
-#pages = pages[:-1]
-max_page = pages[-1]
-
-#end of url; start = ~~ >> '~~' == (PageNum-1) * limit(in this case, 50) 
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
+        for result in results:
+            title = result.find("h2",{"class":"title"}).find("a")["title"]
+            print(title)
+    return jobs
+last_page = extract_indeed_pages()
+#print(last_page)
+extract_indeed_jobs(last_page)
--- 1. Python_Scrapper/1. PythonScrapper.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-from indeed import extract_indeed_pages, extract_indeed_jobs
-
-last_indeed_page = extract_indeed_pages()
-
-extract_indeed_jobs(last_indeed_page)
