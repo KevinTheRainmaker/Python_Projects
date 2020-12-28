30b0cd9a1cb6e1c20cbbff4364a4e5e95b36f833
KangbeenKo <sweetrainforyou@gmail.com>
2020-12-29 00:47:46 +0900

    Add Saram-in

--- 1. PythonScrapper.py
+++ 1. PythonScrapper.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-LIMIT = 50
-URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
+indeed_LIMIT = 50
+saramin_LIMIT = 100
+indeed_URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={indeed_LIMIT}"
+saramin_URL = f"http://www.saramin.co.kr/zf_user/search/recruit?searchType=search&searchword=%ED%8C%8C%EC%9D%B4%EC%8D%AC&recruitPageCount={saramin_LIMIT}"
 
-
-def extract_indeed_pages():
+def extract_pages(URL):
     results = requests.get(URL)
-
     soup = BeautifulSoup(results.text, "html.parser")
     pagination = soup.find("div", {"class": "pagination"})
     links = pagination.find_all('a')
@@ -15,13 +15,9 @@ def extract_indeed_pages():
     for link in links[:-1]:
         pages.append(int(link.string))
     max_page = pages[-1]
-
     return max_page
-    # for n in range(max_page):
-    #     print(INDEED_URL +"&"+ f"start={n*50}")
 
-
-def extract_job(html):
+def extract_indeed_job(html):
     title = html.find("h2", {"class": "title"}).find("a")["title"]
     company = html.find("span", {"class": "company"})
     company_anchor = company.find("a")
@@ -35,20 +31,55 @@ def extract_job(html):
     return {'title': title, 'company': company, "location": location, "link": f"https://kr.indeed.com/viewjob?jk={job_id}"}
 
 
+def extract_saramin_job(html):
+    title = html.find("h2", {"class": "job-tit"}).find("a")["title"]
+    company = html.find("div", {"class": "job_sector"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = company_anchor.string
+    else:
+        company = company.string
+    company = company.strip()
+    location = html.find("span", {"class": "job_condition"})["href"]
+    job_id = html["value"]
+    return {'title': title, 'company': company, "location": location, "link": f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}"}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
-        result = requests.get(f"{URL}&start={page*LIMIT}")
+        result = requests.get(f"{indeed_URL}&start={page*indeed_LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
-            job = extract_job(result)
+            job = extract_indeed_job(result)
             jobs.append(job)
+        print(jobs)
     return jobs
 
 
-last_indeed_page = extract_indeed_pages()
-#print(last_page)
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
-print(indeed_jobs)
+
+
+def extract_saramin_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{saramin_URL}&start={page*saramin_LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "item_recruit visited has_similar_list"})
+        for result in results:
+            job = extract_saramin_job(result)
+            jobs.append(job)
+        print(jobs)
+    return jobs
+
+
+
+
+# def get_indeed_jobs():
+#     last_indeed_page = extract_indeed_pages()
+#     indeed_jobs = extract_indeed_jobs(last_indeed_page)
+#     return indeed_jobs
+
+print(extract_saramin_jobs(extract_pages(saramin_URL)))
