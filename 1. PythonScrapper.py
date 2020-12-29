12eb65d16c5cf9f9d9e201e06ed6f035ac22e8cc
KangbeenKo <sweetrainforyou@gmail.com>
2020-12-29 23:23:19 +0900

    SARAMIN Done

--- 1. PythonScrapper.py
+++ 1. PythonScrapper.py
@@ -30,84 +30,55 @@ def extract_indeed_job(html):
     job_id = html["data-jk"]
     return {'title': title, 'company': company, "location": location, "link": f"https://kr.indeed.com/viewjob?jk={job_id}"}
 
-
-#def extract_saramin_job(html):
-    title = html.find("h2", {"class": "job-tit"}).find("a")["title"]
-    company = html.find("div", {"class": "job_sector"})
+def extract_saramin_job(html):
+    title = html.find("h2", {"class": "job_tit"}).find("a")["title"]
+    company = html.find("div", {"class": "area_corp"})
     company_anchor = company.find("a")
     if company_anchor is not None:
         company = company_anchor.string
     else:
         company = company.string
     company = company.strip()
-    location = html.find("span", {"class": "job_condition"})["href"]
+    location = html.find("div", {"class": "job_condition"}).find("a").string
     job_id = html["value"]
     return {'title': title, 'company': company, "location": location, "link": f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}"}
 
-
 def extract_indeed_jobs(last_page):
     jobs = []
+    print('Start Scrapping from "INDEED"')
     for page in range(last_page):
-        #print(f"Scrapping page {page}")
+        print(f"Scrapping page {page+1}")
         result = requests.get(f"{indeed_URL}&start={page*indeed_LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
             job = extract_indeed_job(result)
             jobs.append(job)
-        #print(jobs)
     return jobs
 
-
-
-
-#def extract_saramin_jobs(last_page):
+def extract_saramin_jobs(last_page):
     jobs = []
+    print('Start Scrapping from "SARAMIN"')
     for page in range(last_page):
-        print(f"Scrapping page {page}")
+        print(f"Scrapping page {page+1}")
         result = requests.get(f"{saramin_URL}&start={page*saramin_LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
-        results = soup.find_all("div", {"class": "item_recruit visited has_similar_list"})
+        results = soup.find_all("div", {"class": "item_recruit"})
         for result in results:
             job = extract_saramin_job(result)
             jobs.append(job)
-        print(jobs)
     return jobs
-
-
-
-
-
-
-
-
-
-
 
 def give_me_job(URL1, URL2):
     last_page_1 = extract_pages(URL1)
     last_page_2 = extract_pages(URL2)
 
     indeed = extract_indeed_jobs(last_page_1)
-    #saramin = extract_saramin_jobs(last_page_2)
-    
-    print(last_page_1)
-    print(last_page_2)
+    print(indeed)
 
+    saramin = extract_saramin_jobs(last_page_2)
+    print(saramin)
+
+
+#Trial
 give_me_job(indeed_URL, saramin_URL)
-
-    
-
-
-
-
-
-
-
-
-# def get_indeed_jobs():
-#     last_indeed_page = extract_indeed_pages()
-#     indeed_jobs = extract_indeed_jobs(last_indeed_page)
-#     return indeed_jobs
-
-#print(extract_saramin_jobs(extract_pages(saramin_URL)))
