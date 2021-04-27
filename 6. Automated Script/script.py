4be83d323485acbe6f26dcba19201207d579e9c1
KangbeenKo <sweetrainforyou@gmail.com>
2021-04-27 09:11:10 +0900

    Add Exception - for unexpected alert pop-up

--- 6. Automated Script/script.py
+++ 6. Automated Script/script.py
@@ -1,11 +1,11 @@
-import time
 import random
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 
-driver = webdriver.Chrome("C:/Users/kgbko/chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome("C:/Users/kgbko1117/chromedriver_win32/chromedriver.exe")
 driver.maximize_window()
 driver.implicitly_wait(20)
 driver.get("https://zeus.gist.ac.kr/sys/main/main.do")
@@ -14,6 +14,9 @@ elem.send_keys('kgbko1117')
 elem = driver.find_element_by_name("login_pw") 
 elem.send_keys('rhrkdqls123!')
 elem.send_keys(Keys.ENTER)
+
+if Alert(driver) == True:
+    Alert(driver).accept()
 
 # 찾고자 하는 요소가 iframe 안에 있어 NotFound Error가 발생
 # 이를 해결하기 위해 'driver.switchTo (). frame ()'명령을 사용하여 iframe으로 전환 후 요소 탐색 진행
@@ -28,7 +31,7 @@ tab.click()
 # input 창을 먼저 클릭해야 해댱 element가 interactable 해지는 구조였기에 발생한 오류
 temp = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_divForm_edtTemp_input"]')
 temp.click()
-temp.send_keys('36.'+ str(random.randrange(2,8))) # For more naturalness  
+temp.send_keys('36.'+str(random.randrange(2,8))) 
 
 save = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_btnSave"]').click()
 
