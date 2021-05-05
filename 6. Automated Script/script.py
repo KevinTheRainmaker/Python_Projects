8cb6ac54cc72786088493a056cb66083fafebb77
KangbeenKo <sweetrainforyou@gmail.com>
2021-05-05 21:31:34 +0900

    Add try-except for exception

--- 6. Automated Script/script.py
+++ 6. Automated Script/script.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 
+time.sleep(random.randrange(1,9)*60)
+
 driver = webdriver.Chrome("C:/Users/kgbko1117/chromedriver_win32/chromedriver.exe")
 driver.maximize_window()
 driver.implicitly_wait(20)
@@ -15,28 +17,32 @@ elem = driver.find_element_by_name("login_pw")
 elem.send_keys('rhrkdqls123!')
 elem.send_keys(Keys.ENTER)
 
-if Alert(driver) == True:
-    Alert(driver).accept()
+while True:
+    try:
+        # 찾고자 하는 요소가 iframe 안에 있어 NotFound Error가 발생
+        # 이를 해결하기 위해 'driver.switchTo (). frame ()'명령을 사용하여 iframe으로 전환 후 요소 탐색 진행
+        element = driver.find_element_by_xpath("//div[@id='ifm_idx_wrap']/iframe")
+        driver.switch_to_frame(element)
 
-# 찾고자 하는 요소가 iframe 안에 있어 NotFound Error가 발생
-# 이를 해결하기 위해 'driver.switchTo (). frame ()'명령을 사용하여 iframe으로 전환 후 요소 탐색 진행
-element = driver.find_element_by_xpath("//div[@id='ifm_idx_wrap']/iframe")
-driver.switch_to_frame(element)
+        page = driver.find_element_by_xpath('//div[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_7_cell_7_0_controltreeTextBoxElement"]')
+        page.click()
+        tab = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_12_cell_12_0_controltreeTextBoxElement"]/div')
+        tab.click()
 
-page = driver.find_element_by_xpath('//div[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_7_cell_7_0_controltreeTextBoxElement"]')
-page.click()
-tab = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_12_cell_12_0_controltreeTextBoxElement"]/div')
-tab.click()
+        # input 창을 먼저 클릭해야 해댱 element가 interactable 해지는 구조였기에 발생한 오류
+        temp = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_divForm_edtTemp_input"]')
+        temp.click()
+        temp.send_keys('36.'+str(random.randrange(2,8))) 
 
-# input 창을 먼저 클릭해야 해댱 element가 interactable 해지는 구조였기에 발생한 오류
-temp = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_divForm_edtTemp_input"]')
-temp.click()
-temp.send_keys('36.'+str(random.randrange(2,8))) 
+        save = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_btnSave"]').click()
 
-save = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_btnSave"]').click()
+        # UnexpectedAlertPresentException: 예기치않은 팝업창 대처 -> Alert import 하여 accept
+        da = Alert(driver)
+        da.accept()
 
-# UnexpectedAlertPresentException: 예기치않은 팝업창 대처 -> Alert import 하여 accept
-da = Alert(driver)
-da.accept()
-
-time.sleep(1)
+        time.sleep(1)
+        break
+    except:
+        Alert(driver).accept()
+        button = driver.find_element_by_partial_link_text('나중에 변경')
+        button.click()
