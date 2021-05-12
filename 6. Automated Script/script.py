4b31b038613149ec32981fb4056ee8292f6277ee
KangbeenKo <sweetrainforyou@gmail.com>
2021-05-13 00:28:49 +0900

    Fix Infinite Message

--- 6. Automated Script/script.py
+++ 6. Automated Script/script.py
@@ -16,7 +16,6 @@ def post_message(token, channel, text):
  
 myToken = "xoxb-2037120074375-2048826409029-8AiMjwr7BabTY0M8m8NM8cvf"
 
-
 time.sleep(random.randrange(1,9)*60)
 
 driver = webdriver.Chrome("C:/Users/kgbko1117/chromedriver_win32/chromedriver.exe")
@@ -29,31 +28,30 @@ elem = driver.find_element_by_name("login_pw")
 elem.send_keys('rhrkdqls123!')
 elem.send_keys(Keys.ENTER)
 
-while True:
-    try:
-        # 찾고자 하는 요소가 iframe 안에 있어 NotFound Error가 발생
-        # 이를 해결하기 위해 'driver.switchTo (). frame ()'명령을 사용하여 iframe으로 전환 후 요소 탐색 진행
-        element = driver.find_element_by_xpath("//div[@id='ifm_idx_wrap']/iframe")
-        driver.switch_to_frame(element)
+try:
+    # 찾고자 하는 요소가 iframe 안에 있어 NotFound Error가 발생
+    # 이를 해결하기 위해 'driver.switchTo (). frame ()'명령을 사용하여 iframe으로 전환 후 요소 탐색 진행
+    element = driver.find_element_by_xpath("//div[@id='ifm_idx_wrap']/iframe")
+    driver.switch_to_frame(element)
 
-        page = driver.find_element_by_xpath('//div[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_7_cell_7_0_controltreeTextBoxElement"]')
-        page.click()
-        tab = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_12_cell_12_0_controltreeTextBoxElement"]/div')
-        tab.click()
+    page = driver.find_element_by_xpath('//div[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_7_cell_7_0_controltreeTextBoxElement"]')
+    page.click()
+    tab = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_leftFrame_form_gridMenu_body_gridrow_12_cell_12_0_controltreeTextBoxElement"]/div')
+    tab.click()
 
-        # input 창을 먼저 클릭해야 해댱 element가 interactable 해지는 구조였기에 발생한 오류
-        temp = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_divForm_edtTemp_input"]')
-        temp.click()
-        temp.send_keys('36.'+str(random.randrange(2,8))) 
+    # input 창을 먼저 클릭해야 해댱 element가 interactable 해지는 구조였기에 발생한 오류
+    temp = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_divForm_edtTemp_input"]')
+    temp.click()
+    temp.send_keys('36.'+str(random.randrange(2,8))) 
 
-        save = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_btnSave"]').click()
+    save = driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_HFrameSet_MDIFrameSet_ctxFrameSet_ctxFrame_PERS07^PERS07_08^005^AmcDailyTempRegE_form_div_sample_divMain_btnSave"]').click()
 
-        # UnexpectedAlertPresentException: 예기치않은 팝업창 대처 -> Alert import 하여 accept
-        da = Alert(driver)
-        da.accept()
+    # UnexpectedAlertPresentException: 예기치않은 팝업창 대처 -> Alert import 하여 accept
+    da = Alert(driver)
+    da.accept()
 
-        time.sleep(1)
-        break
-    except:
-        post_message(myToken,"#BOT","체온측정 확인 필요")
+    time.sleep(1)
+except:
+    post_message(myToken,"#BOT","체온측정 확인 필요")
+    time.sleep(1)
          
